95abcc95fe457935e69e8d1517e9c0f1b0cdc2b9
Saeed ullah <saeed.khan5032@gmail.com>
2024-07-20 05:05:24 -0700

    Version 1

--- app/src/main/java/com/compose/gettingstartedwithcompose/MainActivity.kt
+++ app/src/main/java/com/compose/gettingstartedwithcompose/MainActivity.kt
@@ -21,17 +21,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Call
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -43,12 +51,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.compose.gettingstartedwithcompose.ui.theme.GettingStartedWithComposeTheme
+import com.compose.gettingstartedwithcompose.ui.theme.buttonClick
+import com.compose.gettingstartedwithcompose.ui.theme.counter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,55 +79,47 @@ class MainActivity : ComponentActivity() {
 
 @Preview(
     showBackground = true,
-    showSystemUi = true,
+    showSystemUi = true
 )
 @Composable
-fun MainUi() {
+fun MainUi( ) {
+   var  names:List<String> = listOf("khan","ali")
     GettingStartedWithComposeTheme {
-        counter()
-    }
-}
-
-@Composable
-fun counter() {
-var counter = remember{
-    mutableIntStateOf(1)
-}
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-
-        Text(text = "${counter.value}")
-        Spacer(modifier = Modifier.height(180.dp))
-        buttonClick {
-            counter.value+=100
+        Column(Modifier.padding(horizontal = 10.dp)) {
+        for (name in names){
+            Spacer(modifier = Modifier.height(10.dp))
+                header(
+                    name,
+                    Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.Cyan)
+                        .padding(10.dp)
+                )
+            }
         }
 
     }
 }
 
 @Composable
-fun buttonClick( onclick : ()->Unit){
-    Card(
-        modifier = Modifier
-            .size(50.dp)
-            .clickable {
-                onclick.invoke()
-            },
-        shape = CircleShape,
-        colors = CardDefaults.cardColors(containerColor = Color.Yellow)
-    ) {
-        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = "Tap"
-            )
+fun header(text:String,modifier1: Modifier) {
+  Box(modifier = modifier1)
+  {
+      Row {
+          Column(modifier = Modifier.weight(1f)) {
+              Text(text = text)
+              Text(text = "android")
+          }
 
-        }
-    }
+          ElevatedButton(onClick = { /*TODO*/ }) {
+              Text(text = "Show More")
+
+          }
+      }
+  }
 }
+
+
+
 
 
---
+++ app/src/main/java/com/compose/gettingstartedwithcompose/ui/theme/Second.kt
@@ -0,0 +1,69 @@
+package com.compose.gettingstartedwithcompose.ui.theme
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun counter() {
+    var counter = remember{
+        mutableIntStateOf(1)
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+        Text(text = "${counter.value}")
+        Spacer(modifier = Modifier.height(180.dp))
+        buttonClick {
+            counter.value+=100
+        }
+
+    }
+}
+
+@Composable
+fun buttonClick( onclick : ()->Unit){
+    Card(
+        modifier = Modifier
+            .size(50.dp)
+            .clickable {
+                onclick.invoke()
+            },
+        shape = CircleShape,
+        colors = CardDefaults.cardColors(containerColor = Color.Yellow)
+    ) {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Tap"
+            )
+
+        }
+    }
+}
